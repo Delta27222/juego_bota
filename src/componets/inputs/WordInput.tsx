0dd0b872d1601b022f808e7f80f67f451bc38ca3
Delta27222 <angelgabriel.hernandez@gmail.com>
2024-12-15 22:16:32 -0400

    Adjust WordInput dimensions for improved visual consistency

--- src/componets/inputs/WordInput.tsx
+++ src/componets/inputs/WordInput.tsx
@@ -130,7 +130,7 @@ const WordInput = ({
         value={letrasIngresadas[index]}
         onChange={(event) => handleChange(index, event)}
         onKeyDown={(event) => handleKeyDown(index, event)}
-        className={`border-none text-white w-[30px] h-[30px] md:w-[40px] md:h-[40px] text-center font-bold rounded font-sans ${show}`}
+        className={`border-none text-white w-[15px] h-[15px] md:w-[40px] md:h-[40px] text-center font-bold rounded font-sans ${show}`}
         style={{
           imeMode: "disabled",
           caretColor: "transparent",
