2b806388bc05b700fbde349edb427ab0bd0b778d
Delta27222 <angelgabriel.hernandez@gmail.com>
2024-12-15 21:59:12 -0400

    Improve layout of Surprice component for better alignment and centering

--- src/componets/surprice/Surprice.tsx
+++ src/componets/surprice/Surprice.tsx
@@ -3,9 +3,9 @@ import Confetti from 'react-confetti'
 
 function Surprice() {
   return (
-    <section className='mb-10 '>
+    <section className='mb-10 flex flex-col justify-center items-center'>
       <Confetti width={1500} height={1500} gravity={0.6} />
-      <h1 className='text-4xl text-green-600'>Si, efectivamente la bota le toca a 👇🏻</h1>
+      <h1 className='text-4xl text-green-600 text-center'>Si, efectivamente la bota le toca a 👇🏻</h1>
     </section>
   )
 }
